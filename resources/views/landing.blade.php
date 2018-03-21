25a293634c64c7be920ae2bc51aad8d4f896c5d0
Aaron Verones <aaron.verones@alumni.ubc.ca>
2018-03-21 09:25:57 -0700

    google analytics and hotjar installed

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -18,6 +18,27 @@
         <script src="/js/main.js"></script>
         <script src="/js/fontawesome-all.min.js"></script>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106637379-3"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-106637379-3');
+        </script>
+        <!-- Hotjar Tracking Code for http://aaronverones.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:822546,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+
     </head>
     <body>
     <div id="wrapper">
